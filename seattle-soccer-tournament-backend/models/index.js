97ff1194c8f173df3719a27224ed4b2a65c6674a
bdiallo10 <bdiallo10@icloud.com>
2020-08-30 14:49:45 -0700

    connecting back end to heroku

--- seattle-soccer-tournament-backend/models/index.js
+++ seattle-soccer-tournament-backend/models/index.js
@@ -15,6 +15,9 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+mongoose.connect( process.env.MONGODB_URI || `http://localhost:5000/api/v1` );
+
+
 module.exports = {
     Tournament: require('./tournament'),
     Team: require('./team'),
